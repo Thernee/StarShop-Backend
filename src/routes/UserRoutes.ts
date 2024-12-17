5bf92e41cbc0aa5a11d95c665d7e4f322737d53d
JosephPDF <josephpf12@hotmail.com>
2024-12-17 14:39:04 -0600

    Update file UserRoutes.ts

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -2,16 +2,19 @@ import { Router } from 'express';
 import { createUser, deleteUser, getAllUsers, getUser, updateUser } from '../controllers/UserController';
 import { validationMiddleware } from '../middleware/userValidation.middleware';
 import { CreateUserDto, UpdateUserDto } from '../dtos/UserDTO';
-
-
+import { sessionMiddleware } from '../middleware/session.middleware'; 
 
 const router = Router();
 
-router.get('/', getAllUsers);
+// Route to get all users
+router.get('/', sessionMiddleware, getAllUsers); 
+// Route to create a new user with validation
 router.post('/create', validationMiddleware(CreateUserDto), createUser);
-router.get('/show/:id', getUser);
-router.put('/update/:id',validationMiddleware(UpdateUserDto), updateUser);
-router.delete('/delete/:id', deleteUser);
-
+// Route to get a specific user by ID
+router.get('/show/:id', sessionMiddleware, getUser); 
+// Route to update a user by ID with validation
+router.put('/update/:id', sessionMiddleware, validationMiddleware(UpdateUserDto), updateUser); 
+// Route to delete a user by ID
+router.delete('/delete/:id', sessionMiddleware, deleteUser); 
 
 export default router;
