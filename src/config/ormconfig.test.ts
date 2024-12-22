05d57667b5e04fe037b17077697fa9c56eee62c5
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-21 21:35:19 -0600

    Fix: updated orm config test

--- src/config/ormconfig.test.ts
+++ src/config/ormconfig.test.ts
@@ -1,10 +1,15 @@
 import { DataSource } from 'typeorm';
 import { TestEntity } from '../entities/testEntity';
 import { User } from '../entities/User';
+import { Product } from '../entities/Product';
+import { ProductType } from '../entities/ProductType';
+import { ProductVariant } from '../entities/ProductVariant';
+import { Attribute } from '../entities/Attribute';
+import { AttributeValue } from '../entities/AttributeValue';
 
 export const testDataSource = new DataSource({
   type: 'sqlite',
   database: ':memory:',
   synchronize: true,
-  entities: [TestEntity, User]
+  entities: [TestEntity, User, Product, ProductType, ProductVariant, Attribute, AttributeValue]
 });
