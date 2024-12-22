15fe75e7f5804537609fce18a613c7a6e69fb62e
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-21 21:37:01 -0600

    Fix: update setupDb function

--- src/utils/test-utils.ts
+++ src/utils/test-utils.ts
@@ -4,8 +4,8 @@ export const setupTestDB = async () => {
   if (!testDataSource.isInitialized) {
     await testDataSource.initialize();
   }
+  await testDataSource.synchronize(true); // Sync
 };
-
 export const teardownTestDB = async () => {
   if (testDataSource.isInitialized) {
     await testDataSource.destroy();
