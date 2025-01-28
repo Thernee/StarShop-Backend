6c504f93e78202701112ac8f0ed88c56f3816e95
Santiago Villarreal Arley <115122095+Villarley@users.noreply.github.com>
2025-01-28 11:23:35 -0600

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import userRoutes from './UserRoutes';
-// import productRoutes from './product.routes';
-// import storeRoutes from './store.routes';
+import productRoutes from './product.routes';
+import storeRoutes from './store.routes';
 
 const router = Router();
 
@@ -12,7 +12,7 @@ router.get('/health', (req, res) => {
 
 // Mount route modules
 router.use('/users', userRoutes);
-// router.use('/products', productRoutes);
-// router.use('/stores', storeRoutes);
+router.use('/products', productRoutes);
+router.use('/stores', storeRoutes);
 
 export default router;
